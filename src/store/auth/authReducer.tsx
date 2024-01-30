2c1cdcf950c0a96c45812920e6696c3b4070fbf7
Kennedy Kithome <kithomeken@gmail.com>
2024-01-30 19:45:24 +0300

    Revoke Authenticated Session - Depreciated

--- src/store/auth/authReducer.tsx
+++ src/store/auth/authReducer.tsx
@@ -48,7 +48,7 @@ export const authReducer = (state = initialState, action: any) => {
                 error: "The provided credentials are incorrect.",
             }
 
-        case AUTH_.REVOKE_SESSION:
+        case AUTH_.REVOKE_SESSIO:
             CookieServices.remove(COOKIE_KEYS.SANCTUM)
             CookieServices.remove(COOKIE_KEYS.UAID)
             StorageServices.removeLocalStorage(STORAGE_KEYS.ACCOUNT_DATA)
