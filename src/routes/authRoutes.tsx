08c94c1bee6cbce3be15a9c17d978dd64ce6e0dd
Kennedy Kithome <kithomeken@gmail.com>
2024-02-02 13:09:18 +0300

    Yeat Temp Route

--- src/routes/authRoutes.tsx
+++ src/routes/authRoutes.tsx
@@ -2,10 +2,12 @@ import { TipMe } from "../views/home/TipMe";
 import { SignIn } from "../views/auth/SignIn";
 import { SignUp } from "../views/auth/SignUp";
 import { Routes_Interface } from "../lib/modules/routesInterface";
+import { Yeat } from "../views/home/Yeat";
 
 export const authenticationRoutes: Array<Routes_Interface> = [
     { path: "/auth/sign-in", element: <SignIn />, caseSensitive: true, name: 'AUTH_SIGN_IN' },
     { path: "/auth/sign-up", element: <SignUp />, caseSensitive: true, name: 'AUTH_SIGN_UP' },
     { path: "/artist/:acid", element: <TipMe />, caseSensitive: true, name: 'AUTH_SIGN_UP' },
 
+    { path: "/yeat", element: <Yeat />, caseSensitive: true, name: 'YEAT_' },
 ]
