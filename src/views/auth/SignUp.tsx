2eec6d60a7ab2b350593e16d4c58d09d46b53f3a
Kennedy Kithome <kithomeken@gmail.com>
2024-02-13 07:55:16 +0300

    Local Storage Purge

--- src/views/auth/SignUp.tsx
+++ src/views/auth/SignUp.tsx
@@ -58,11 +58,6 @@ export const SignUp = () => {
     )?.path
 
     React.useEffect(() => {
-        if (location.search !== null || location.search !==  undefined) {
-            const searchParams:any  = parseQueryString(location.search);
-            StorageServices.setLocalStorage(STORAGE_KEYS.eede, searchParams.hash)
-        }
-        
         authRedirectResult()
             .then(async (result) => {
                 if (!result) {
