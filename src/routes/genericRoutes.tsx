382d2d48070f8706190f09f04b60277de53436f8
Kennedy Kithome <kithomeken@gmail.com>
2024-03-17 19:42:08 +0300

    Common Routes Implementation

--- src/routes/genericRoutes.tsx
+++ src/routes/genericRoutes.tsx
@@ -1,6 +1,20 @@
 import { EmailActions } from "../views/auth/EmailActions";
 import { Routes_Interface } from "../lib/modules/routesInterface";
+import { Yeat } from "../views/home/Yeat";
+import { IdentityCheck } from "../views/home/IdentityCheck";
 
 export const genericRoutes: Array<Routes_Interface> = [
     { path: "_/auth/action/email", element: <EmailActions />, caseSensitive: true, name: 'EMAIL_ACTION_' },
+
+
+    { path: "/yeat", element: <Yeat />, caseSensitive: true, name: 'YEAT_' },
+]
+
+export const commonRoutes: Array<Routes_Interface> = [
+    { 
+        path: "/home", 
+        element: <IdentityCheck />, 
+        caseSensitive: true, 
+        name: 'HOME_'
+    },
 ]
