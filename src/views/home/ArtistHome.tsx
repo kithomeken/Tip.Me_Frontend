7a96783a8e793fc9c887fe087d27bd8a5621ab00
Kennedy Kithome <kithomeken@gmail.com>
2024-03-17 21:46:58 +0300

    Padding Change

--- src/views/home/ArtistHome.tsx
+++ src/views/home/ArtistHome.tsx
@@ -151,142 +151,144 @@ export const ArtistHome = () => {
                 <title>{state.pageTitle}</title>
             </Helmet>
 
-            {
-                state.status === 'rejected' ? (
-                    <div className="py-3 px-4 w-full">
-                        <div className="flex items-center justify-center">
-                            {
-                                state.httpStatus === 404 ? (
-                                    <ERR_404
-                                        compact={true}
-                                    />
-                                ) : (
-                                    <ERR_500 />
-                                )
-                            }
+            <div className="w-full px-4 py-4">
+                {
+                    state.status === 'rejected' ? (
+                        <div className="py-3 px-4 w-full">
+                            <div className="flex items-center justify-center">
+                                {
+                                    state.httpStatus === 404 ? (
+                                        <ERR_404
+                                            compact={true}
+                                        />
+                                    ) : (
+                                        <ERR_500 />
+                                    )
+                                }
+                            </div>
                         </div>
-                    </div>
-                ) : state.status === 'fulfilled' ? (
-                    <div className="w-full">
-                        <div className={`w-full mb-3`}>
-                            <div className="kiOAkj" style={CONFIG_MAX_WIDTH}>
-                                <div className="w-full flex flex-col-reverse md:space-x-4 md:flex-row pb-6">
-                                    <div className="flex-none flex flex-col justify-center border-t md:border-t-0 md:pt-0 pt-3">
-                                        <img src={qrCode.lowQuality} alt="qr_code" className="block text-center m-auto" />
+                    ) : state.status === 'fulfilled' ? (
+                        <div className="w-full">
+                            <div className={`w-full mb-3`}>
+                                <div className="kiOAkj" style={CONFIG_MAX_WIDTH}>
+                                    <div className="w-full flex flex-col-reverse md:space-x-4 md:flex-row pb-6">
+                                        <div className="flex-none flex flex-col justify-center border-t md:border-t-0 md:pt-0 pt-3">
+                                            <img src={qrCode.lowQuality} alt="qr_code" className="block text-center m-auto" />
 
-                                        <a className="text-amber-600 w-40 py-2 m-auto px-4 flex flex-row items-center justify-center border border-amber-600 md:hidden text-sm text-center rounded-md bg-white hover:bg-amber-700 focus:outline-none" href={qrCode.highQuality} download={state.data.qrCodeImageName}>
-                                            <i className="fa-duotone fa-download mr-2 fa-lg"></i>
-                                            Download
-                                        </a>
-                                    </div>
+                                            <a className="text-amber-600 w-40 py-2 m-auto px-4 flex flex-row items-center justify-center border border-amber-600 md:hidden text-sm text-center rounded-md bg-white hover:bg-amber-700 focus:outline-none" href={qrCode.highQuality} download={state.data.qrCodeImageName}>
+                                                <i className="fa-duotone fa-download mr-2 fa-lg"></i>
+                                                Download
+                                            </a>
+                                        </div>
 
-                                    <div className="flex-grow border-0 md:border-l md:ml-3 px-4 pb-4 md:py-0">
-                                        <div className="w-full">
-                                            <div className="w-full flex flex-row items-center -middle">
-                                                <span className="py-1 flex-grow px-1.5 block text-2xl text-amber-600 mb-2 capitalize">
-                                                    {state.data.entity.name}
+                                        <div className="flex-grow border-0 md:border-l md:ml-3 px-4 pb-4 md:py-0">
+                                            <div className="w-full">
+                                                <div className="w-full flex flex-row items-center -middle">
+                                                    <span className="py-1 flex-grow px-1.5 block text-2xl text-amber-600 mb-2 capitalize">
+                                                        {state.data.entity.name}
+                                                    </span>
+
+                                                    {
+                                                        state.data.entity.nominated === 'Y' ? (
+                                                            <button type="button" onClick={showOrHideWithdrawModal} className="bg-amber-600 flex-none w-40 py-2 px-4 float-right hidden text-sm md:flex flex-row items-center justify-center text-center rounded-md text-white hover:bg-amber-700 focus:outline-none">
+                                                                <i className="fa-light fa-money-bill-wave mr-2 fa-lg"></i>
+                                                                Withdraw
+                                                            </button>
+                                                        ) : null
+                                                    }
+                                                </div>
+
+                                                <span className="px-1.5 block text-sm text-stone-500 mb-2 capitalize">
+                                                    {state.data.entity.account}
                                                 </span>
+
+                                                <span className=" py-1 px-1.5 block text-sm text-slate-700">
+                                                    <i className="fa-light fa-wallet text-slate-500 fa-lg mr-2"></i>
+                                                    Wallet
+                                                </span>
+
+                                                <div className="w-full flex flex-row align-middle items-center py-1">
+                                                    <span className=" py-1 px-1.5 text-slate-500 text-xs">
+                                                        Ksh.
+                                                    </span>
+
+                                                    <span className=" py-1 px-1.5 text-3xl">
+                                                        <span className="text-slate-700">{state.data.entity.bal.split('.')[0]}</span>
+                                                        <span className="text-slate-400">.{state.data.entity.bal.split('.')[1]}</span>
+                                                    </span>
+                                                </div>
 
                                                 {
                                                     state.data.entity.nominated === 'Y' ? (
-                                                        <button type="button" onClick={showOrHideWithdrawModal} className="bg-amber-600 flex-none w-40 py-2 px-4 float-right hidden text-sm md:flex flex-row items-center justify-center text-center rounded-md text-white hover:bg-amber-700 focus:outline-none">
-                                                            <i className="fa-light fa-money-bill-wave mr-2 fa-lg"></i>
-                                                            Withdraw
-                                                        </button>
+                                                        <div className="flex flex-col justify-center py-2">
+                                                            <button type="button" onClick={showOrHideWithdrawModal} className="text-amber-600 py-2 px-4 sm:hidden text-sm flex flex-row border border-amber-600 items-center justify-center text-center rounded-md bg-white hover:bg-amber-700 focus:outline-none">
+                                                                <i className="fa-light fa-money-bill-wave mr-2 fa-lg"></i>
+                                                                Withdraw
+                                                            </button>
+                                                        </div>
                                                     ) : null
                                                 }
-                                            </div>
 
-                                            <span className="px-1.5 block text-sm text-stone-500 mb-2 capitalize">
-                                                {state.data.entity.account}
-                                            </span>
-
-                                            <span className=" py-1 px-1.5 block text-sm text-slate-700">
-                                                <i className="fa-light fa-wallet text-slate-500 fa-lg mr-2"></i>
-                                                Wallet
-                                            </span>
-
-                                            <div className="w-full flex flex-row align-middle items-center py-1">
-                                                <span className=" py-1 px-1.5 text-slate-500 text-xs">
-                                                    Ksh.
-                                                </span>
-
-                                                <span className=" py-1 px-1.5 text-3xl">
-                                                    <span className="text-slate-700">{state.data.entity.bal.split('.')[0]}</span>
-                                                    <span className="text-slate-400">.{state.data.entity.bal.split('.')[1]}</span>
-                                                </span>
-                                            </div>
-
-                                            {
-                                                state.data.entity.nominated === 'Y' ? (
-                                                    <div className="flex flex-col justify-center py-2">
-                                                        <button type="button" onClick={showOrHideWithdrawModal} className="text-amber-600 py-2 px-4 sm:hidden text-sm flex flex-row border border-amber-600 items-center justify-center text-center rounded-md bg-white hover:bg-amber-700 focus:outline-none">
-                                                            <i className="fa-light fa-money-bill-wave mr-2 fa-lg"></i>
-                                                            Withdraw
-                                                        </button>
-                                                    </div>
-                                                ) : null
-                                            }
-
-                                            <div className="flex flex-col justify-center md:py-2">
-                                                <a className="bg-amber-600 w-40 py-2 px-4 hidden text-sm md:flex flex-row items-center justify-center text-center rounded-md text-white hover:bg-amber-700 focus:outline-none" href={qrCode.highQuality} download={state.data.qrCodeImageName}>
-                                                    <i className="fa-duotone fa-download mr-2 fa-lg"></i>
-                                                    Download
-                                                </a>
+                                                <div className="flex flex-col justify-center md:py-2">
+                                                    <a className="bg-amber-600 w-40 py-2 px-4 hidden text-sm md:flex flex-row items-center justify-center text-center rounded-md text-white hover:bg-amber-700 focus:outline-none" href={qrCode.highQuality} download={state.data.qrCodeImageName}>
+                                                        <i className="fa-duotone fa-download mr-2 fa-lg"></i>
+                                                        Download
+                                                    </a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div className="w-full mb-3 px-3">
-                                    <div className="w-full flex flex-row">
-                                        <div className="md:flex-none cursor-pointer basis-1/2 border-b" onClick={() => activateTab('in')}>
-                                            <button className={classNames(
-                                                state.activeTab === 'in' ? 'text-amber-700 bg-amber-200 border-amber-400' : 'hover:text-gray-700 text-gray-500 hover:bg-gray-100 ',
-                                                "text-sm items-center block p-2 px-6 rounded-t rounded-b-none text-center w-full md:w-auto"
-                                            )}>
-                                                <span className="lolrtn robot">
-                                                    Money In
-                                                </span>
-                                            </button>
+                                    <div className="w-full mb-3 px-3">
+                                        <div className="w-full flex flex-row">
+                                            <div className="md:flex-none cursor-pointer basis-1/2 border-b" onClick={() => activateTab('in')}>
+                                                <button className={classNames(
+                                                    state.activeTab === 'in' ? 'text-amber-700 bg-amber-200 border-amber-400' : 'hover:text-gray-700 text-gray-500 hover:bg-gray-100 ',
+                                                    "text-sm items-center block p-2 px-6 rounded-t rounded-b-none text-center w-full md:w-auto"
+                                                )}>
+                                                    <span className="lolrtn robot">
+                                                        Money In
+                                                    </span>
+                                                </button>
+                                            </div>
+
+                                            <div className="md:flex-none cursor-pointer basis-1/2 border-b" onClick={() => activateTab('out')}>
+                                                <button className={classNames(
+                                                    state.activeTab === 'out' ? 'text-amber-700 bg-amber-200 border-amber-400' : 'hover:text-gray-700 text-gray-500 hover:bg-gray-100 ',
+                                                    "text-sm items-center block p-2 px-6 rounded-t rounded-b-none text-center w-full md:w-auto"
+                                                )}>
+                                                    <span className="lolrtn robot">Money Out</span>
+                                                </button>
+                                            </div>
+
+                                            <div className="flex-grow border-b md:block hidden">
+
+                                            </div>
                                         </div>
 
-                                        <div className="md:flex-none cursor-pointer basis-1/2 border-b" onClick={() => activateTab('out')}>
-                                            <button className={classNames(
-                                                state.activeTab === 'out' ? 'text-amber-700 bg-amber-200 border-amber-400' : 'hover:text-gray-700 text-gray-500 hover:bg-gray-100 ',
-                                                "text-sm items-center block p-2 px-6 rounded-t rounded-b-none text-center w-full md:w-auto"
-                                            )}>
-                                                <span className="lolrtn robot">Money Out</span>
-                                            </button>
-                                        </div>
-
-                                        <div className="flex-grow border-b md:block hidden">
-
+                                        <div className="w-full px-3">
+                                            {loadRespectiveTab(state.activeTab)}
                                         </div>
                                     </div>
 
-                                    <div className="w-full px-3">
-                                        {loadRespectiveTab(state.activeTab)}
+                                    <div className="flex mb-4 items-center">
+                                        <p className="text-2xl flex-auto text-amber-600">
+
+                                        </p>
+
+
                                     </div>
-                                </div>
-
-                                <div className="flex mb-4 items-center">
-                                    <p className="text-2xl flex-auto text-amber-600">
-
-                                    </p>
-
 
                                 </div>
-
                             </div>
                         </div>
-                    </div>
-                ) : (
-                    <div className="w-full h-screen -mt-20 flex flex-col justify-center align-middle items-center mx-4">
-                        <Loading />
-                    </div>
-                )
-            }
+                    ) : (
+                        <div className="w-full h-screen -mt-20 flex flex-col justify-center align-middle items-center mx-4">
+                            <Loading />
+                        </div>
+                    )
+                }
+            </div>
 
             {
                 state.status === 'fulfilled' ? (
