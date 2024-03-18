1eaa7a2f0a0db87697082ec23d4040ad6f1e4460
Kennedy Kithome <kithomeken@gmail.com>
2024-03-18 17:17:26 +0300

    Entity Pay In/Contribution Reloacation

--- src/routes/genericRoutes.tsx
+++ src/routes/genericRoutes.tsx
@@ -1,20 +1,31 @@
-import { EmailActions } from "../views/auth/EmailActions";
-import { Routes_Interface } from "../lib/modules/routesInterface";
 import { Yeat } from "../views/home/Yeat";
+import { EntityPayIn } from "../views/home/EntityPayIn";
+import { EmailActions } from "../views/auth/EmailActions";
 import { IdentityCheck } from "../views/home/IdentityCheck";
+import { Routes_Interface } from "../lib/modules/routesInterface";
 
 export const genericRoutes: Array<Routes_Interface> = [
-    { path: "_/auth/action/email", element: <EmailActions />, caseSensitive: true, name: 'EMAIL_ACTION_' },
-
+    {
+        path: "_/auth/action/email",
+        element: <EmailActions />,
+        caseSensitive: true,
+        name: 'EMAIL_ACTION_'
+    },
+    {
+        path: "/entity/:uuid",
+        element: <EntityPayIn />,
+        caseSensitive: true,
+        name: 'ENTITY_0_'
+    },
 
     { path: "/yeat", element: <Yeat />, caseSensitive: true, name: 'YEAT_' },
 ]
 
 export const commonRoutes: Array<Routes_Interface> = [
-    { 
-        path: "/home", 
-        element: <IdentityCheck />, 
-        caseSensitive: true, 
+    {
+        path: "/home",
+        element: <IdentityCheck />,
+        caseSensitive: true,
         name: 'HOME_'
     },
 ]
