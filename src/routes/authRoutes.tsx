f0c7e936af8cc5d29b4cc560a54a0d8161ec95db
Kennedy Kithome <kithomeken@gmail.com>
2024-03-18 17:13:59 +0300

    Post Authentication Routes

--- src/routes/authRoutes.tsx
+++ src/routes/authRoutes.tsx
@@ -3,14 +3,34 @@ import { SignIn } from "../views/auth/SignIn";
 import { SignUp } from "../views/auth/SignUp";
 import { Invitation } from "../views/auth/Invitation";
 import { Routes_Interface } from "../lib/modules/routesInterface";
+import { PostAuthentication } from "../views/auth/PostAuthentication";
 
 export const authenticationRoutes: Array<Routes_Interface> = [
-    { path: "/auth/sign-in", element: <SignIn />, caseSensitive: true, name: 'AUTH_SIGN_IN' },
-    { path: "/auth/sign-up", element: <SignUp />, caseSensitive: true, name: 'AUTH_SIGN_UP' },
-    { path: "/auth/post", element: <SignUp />, caseSensitive: true, name: 'AUTH_SIGN_UP' },
-    { path: "/auth/invitation/:hash", element: <Invitation />, caseSensitive: true, name: 'AUTH_INVITE_' },
+    { 
+        path: "/auth/sign-in", 
+        element: <SignIn />, 
+        caseSensitive: true, 
+        name: 'AUTH_SIGN_IN' 
+    },
+    { 
+        path: "/auth/sign-up", 
+        element: <SignUp />, 
+        caseSensitive: true, 
+        name: 'AUTH_SIGN_UP' 
+    },
+    { 
+        path: "/auth/invitation/:hash", 
+        element: <Invitation />, 
+        caseSensitive: true, 
+        name: 'AUTH_INVITE_' 
+    },
+]
 
-
-    { path: "/entity/:uuid", element: <EntityPayIn />, caseSensitive: true, name: 'ENTITY_0_' },
-
+export const postAuthRoutes: Array<Routes_Interface> = [
+    { 
+        path: "/auth/_/identity/check", 
+        element: <PostAuthentication />, 
+        caseSensitive: true, 
+        name: 'AUTH_IDENTITY_' 
+    },
 ]
