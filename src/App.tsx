1891e50b20832330a8a062fb2aeb0367f9f05812
Kennedy Kithome <kithomeken@gmail.com>
2024-03-18 17:18:41 +0300

    Post Authentication Route Guard Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,21 @@ import "react-toastify/dist/ReactToastify.css"
 
 import './firebase/firebaseConfigs'
 import { ERR_404 } from './views/errors/ERR_404'
-import { commonRoutes, genericRoutes } from "./routes/genericRoutes"
 import EncryptionKeys from "./security/EncryptionKeys"
 import { standardRoutes } from './routes/standardRoutes'
 import AuthRoutesGuard from './lib/guards/AuthRoutesGuard'
-import { authenticationRoutes } from './routes/authRoutes'
 import { standardErrorRoutes } from "./routes/errorRoutes"
 import { administrativeRoutes } from "./routes/adminRoutes"
 import ErrorRoutesGuard from "./lib/guards/ErrorRoutesGuard"
 import GenericRoutesGuard from "./lib/guards/GenericRoutesGuard"
+import PostAuthRouteGuard from "./lib/guards/PostAuthRouteGuard"
 import StandardRoutesGuard from "./lib/guards/StandardRoutesGuard"
 import { CommonRoutesGuard } from "./lib/guards/CommonRoutesGuard"
 import { FULLY_QUALIFIED_DOMAIN_NAME } from "./api/API_Controller"
 import { sanctumAxiosInstance } from "./lib/modules/HelperFunctions"
+import { commonRoutes, genericRoutes } from "./routes/genericRoutes"
 import CoreSettingsRouteGuard from "./lib/guards/CoreSettingsRoutesGuard"
+import { authenticationRoutes, postAuthRoutes } from './routes/authRoutes'
 import { AUTH, AUTH_SIGN_OUT, CSRF_COOKIE_ROUTE } from "./api/API_Registry"
 
 interface RouteContextType {
@@ -74,6 +75,19 @@ function App() {
                     <Routes>
                         <Route element={<AuthRoutesGuard />}>
                             {authenticationRoutes.map((route, index) => {
+                                return (
+                                    <Route
+                                        path={route.path}
+                                        element={route.element}
+                                        key={index}
+                                    />
+                                )
+                            })
+                            }
+                        </Route>
+                        
+                        <Route element={<PostAuthRouteGuard />}>
+                            {postAuthRoutes.map((route, index) => {
                                 return (
                                     <Route
                                         path={route.path}
