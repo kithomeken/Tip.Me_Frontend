2f544ec4aa91fbbf0156565e6d5c4ad1a511ea02
Kennedy Kithome <kithomeken@gmail.com>
2024-03-26 15:42:47 +0300

    Standard Settings Routes

--- src/routes/standardRoutes.tsx
+++ src/routes/standardRoutes.tsx
@@ -3,6 +3,9 @@ import { IdentityOnboarding } from "../views/home/IdentityOnboarding";
 import { Routes_Interface } from "../lib/modules/routesInterface";
 import { WithdrawalRequest } from "../views/home/WithdrawalRequest";
 import { AccountManagement } from "../views/settings/AccountManagement";
+import { ChangeEmail } from "../views/settings/ChangeEmail";
+import { EntityProfile } from "../views/settings/EntityProfile";
+import { Entity } from "../views/settings/Entity";
 
 export const standardRoutes: Array<Routes_Interface> = [
     { 
@@ -20,7 +23,36 @@ export const standardRoutes: Array<Routes_Interface> = [
     
 
 
+
+
+
     { path: "/admin/withdrawals/requests/:uuid", element: <WithdrawalRequest />, activeMenu: 'Y', caseSensitive: true, name: 'PENDING_DETAILS_' },
     { path: "/u/default/account/profile", element: <AccountManagement />, activeMenu: 'Y', caseSensitive: true, name: 'ACCOUNT_PROFILE' },
+
+]
+
+
+export const standardSettingsRoutes: Array<Routes_Interface> = [
+    {
+        path: "/u/settings/account/profile", 
+        element: <EntityProfile />, 
+        activeMenu: 'Y', 
+        caseSensitive: true, 
+        name: 'CNF_ACC_ID_' 
+    },
+    {
+        path: "/u/settings/email/change", 
+        element: <ChangeEmail />, 
+        activeMenu: 'Y', 
+        caseSensitive: true, 
+        name: 'CNF_EMAIL_CHNG_' 
+    },
+    {
+        path: "/u/settings/entity", 
+        element: <Entity />, 
+        activeMenu: 'Y', 
+        caseSensitive: true, 
+        name: 'CNF_ENTITY_' 
+    },
 
 ]
