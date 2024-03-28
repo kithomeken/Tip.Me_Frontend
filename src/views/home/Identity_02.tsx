3e904e36ab13c337a010c14e9f5541bf3ee048f2
Kennedy Kithome <kithomeken@gmail.com>
2024-03-28 08:50:16 +0300

    Identity 02 Redesign

--- src/views/home/Identity_02.tsx
+++ src/views/home/Identity_02.tsx
@@ -1,10 +1,12 @@
+import { toast } from "react-toastify"
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
 import PhoneInput, { isValidPhoneNumber, parsePhoneNumber } from 'react-phone-number-input'
 
 import { useAppSelector } from "../../store/hooks"
 import '../../assets/css/react_phone_number_input.css'
-import serviceCenter from "../../assets/images/13105982_5146757.jpg"
+import { APPLICATION, CONFIG_MAX_WIDTH } from "../../global/ConstantsRegistry"
+import serviceCenter from "../../assets/images/306d5d0d0d19094f8a82a61578f3e9a9.svg"
 import { addMSISDN_ToProfile, resetIdentity } from "../../store/identityCheckActions"
 
 export const Identity_02 = () => {
@@ -44,9 +46,17 @@ export const Identity_02 = () => {
             let { errors } = state
             const validPhone = isValidPhoneNumber(e.target.value)
             const phoneNumber = parsePhoneNumber(e.target.value)
-            
+
             if (phoneNumber) {
-                // console.log('wrvihni43902', phoneNumber.country);
+                if (phoneNumber.country !== 'KE') {
+                    errors.msisdn = 'Only (KE) phone numbers allowed'
+
+                    setstate({
+                        ...state, errors
+                    })
+
+                    return
+                }
             }
 
             if (!validPhone) {
@@ -65,6 +75,33 @@ export const Identity_02 = () => {
         e.preventDefault()
 
         if (!idC_State.processing) {
+            let {input} = state
+            let {errors} = state
+
+            if (input.msisdn.length < 13) {
+                errors.msisdn = "Kindly add a valid phone number"
+
+                setstate({
+                    ...state, errors
+                })
+
+                return
+            }
+
+            if (errors.msisdn.length > 1) {
+                toast.warning("Kindly clear the errors before proceeding", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+
+                return
+            }            
+
             const identProps = {
                 dataDump: {
                     msisdn: state.input.msisdn,
@@ -77,81 +114,104 @@ export const Identity_02 = () => {
 
     return (
         <React.Fragment>
-            <div className="w-full border-dashed rounded-md border-2 border-slate-300">
-                <div className="flex mb-4 w-full flex-col md:flex-row px-3 gap-4 py-3 align-middle justitfy-center m-auto ">
-                    <div className="mx-auto md:basis-1/2 md:px-2 flex-shrink-0 flex items-center justify-center mb-3 sm:mx-0 md:w-64 w-48">
-                        <img src={serviceCenter} alt={serviceCenter} width="auto" className="block text-center m-auto" />
-                    </div>
+            <div className="wrapper w-full overflow-auto h-screen">
+                <section className="gx-container h-screen rounded-md w-full flex items-center justify-center" style={CONFIG_MAX_WIDTH}>
+                    <div className="flex md:flex-row flex-col align-middle items-center w-full md:pb-0 pb-10">
+                        <div className="md:basis-3/5 md:px-6 px-8 w-full h-screen py-8">
+                            <span className="text-2xl self-start text-amber-500 tracking-wider leading-7 block mb-2 md:pt-0 pt-4">{APPLICATION.NAME}</span>
 
-                    <div className="w-full md:basis-1/2">
-                        <div className="text-center md:text-start">
-                            <span className="text-amber-600 mb-2 py-1 md:px-3 text-right block text-sm">
-                                2 OF 4 COMPLETE
-                            </span>
+                            <div className="flex flex-row w-full align-middle justitfy-between items-center md:hidden">
+                                <div className="w-48 pt-4 mx-auto pb-3">
+                                    <img src={serviceCenter} alt={"pana_calling"} width="auto" className="block text-center m-auto" />
+                                </div>
+                            </div>
 
-                            <span className="text-stone-600 mb-3 text-2xl block">
-                                Contact Information
-                                <span className="text-sm text-slate-500 block">
-                                    {/* Stand out from the crowd! */}
-                                </span>
-                            </span>
+                            <div className="w-32 md:float-start float-right">
+                                <div className="w-full py-4 grid grid-cols-3 gap-x-2">
+                                    <div className="rounded-md h-2 shadow-lg bg-amber-600"></div>
+                                    <div className="rounded-md h-2 shadow-lg bg-amber-600"></div>
+                                    <div className="rounded-md h-2 shadow-lg bg-amber-400"></div>
+                                </div>
 
-                            <div className="text-sm text-slate-600 pb-4">
-                                <span className="block">
-                                    Our way of communicating important information such as updates, or security alerts.
+                                <span className="text-sm text-stone-500 md:text-start text-right block">
+                                    2 of 3
                                 </span>
                             </div>
+
+                            <div className="w-full text-sm text-stone-600 float-right mb-3">
+                                <span className="block py-4 text-xl md:text-2xl">
+                                    Secure Your Account
+
+                                    <span className="text-sm pt-4 pb-2 text-stone-500 block">
+                                        Add your phone number for secure cash withdrawals and easy contact in case of issues.
+                                    </span>
+                                </span>
+                            </div>
+
+                            <div className="md:w-3/5 flex flex-col w-full mb-4">
+                                <form className="space-y-4 w-full" onSubmit={msisdnFormHandler}>
+                                    <div className="w-full">
+                                        <label htmlFor="msisdn" className="block text-sm leading-6 text-gray-500 mb-2">Phone Number:</label>
+
+                                        <PhoneInput
+                                            international
+                                            defaultCountry='KE'
+                                            className="border border-gray-300 px-3 py-1 rounded"
+                                            placeholder="Enter phone number"
+                                            value={state.input.msisdn}
+                                            onChange={onPhoneInputChange}
+                                            onBlur={onPhoneInputBlur}
+                                            error={state.input.msisdn ? (isValidPhoneNumber(state.input.msisdn) ? undefined : 'Invalid phone number') : 'Phone number required'}
+                                        />
+
+                                        {
+                                            state.errors.msisdn.length > 0 &&
+                                            <span className='invalid-feedback text-xs text-red-600 pl-0'>
+                                                {state.errors.msisdn}
+                                            </span>
+                                        }
+
+                                        {
+                                            idC_State.error && (
+                                                <span className='invalid-feedback text-xs text-red-600 pl-0'>
+                                                    {idC_State.error}
+                                                </span>
+                                            )
+                                        }
+                                    </div>
+
+                                    <div className="mb-3 pt-3 px-0">
+                                        <button className="bg-amber-600 float-right relative w-28 py-1.5 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:bg-amber-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:bg-amber-700" type="submit">
+                                            {
+                                                idC_State.processing ? (
+                                                    <i className="fad fa-spinner-third fa-xl fa-spin py-2.5"></i>
+                                                ) : (
+                                                    <div className="flex justify-center align-middle items-center gap-x-3">
+                                                        Next
+                                                        <i className="fa-duotone fa-arrow-right fa-lg"></i>
+                                                    </div>
+                                                )
+                                            }
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div className="mx-auto pb-3 pt-10 text-center">
+                                <p className="text-sm text-stone-500">
+                                    © {new Date().getFullYear()}. Elevated Acts of Appreciation, <span className="text-amber-600 block">Tip by Tip.</span>
+                                </p>
+                            </div>
+
+
+
                         </div>
 
-                        <form className="space-y-4 shadow-none px- mb-3 w-full md:w-2/3" onSubmit={msisdnFormHandler}>
-                            <div className="w-full">
-                                <label htmlFor="msisdn" className="block text-sm leading-6 text-gray-500 mb-2">Phone Number:</label>
-
-                                <PhoneInput
-                                    international
-                                    defaultCountry='KE'
-                                    className="border border-gray-300 px-3 py-1.5 rounded"
-                                    placeholder="Enter phone number"
-                                    value={state.input.msisdn}
-                                    onChange={onPhoneInputChange}
-                                    onBlur={onPhoneInputBlur}
-                                    error={state.input.msisdn ? (isValidPhoneNumber(state.input.msisdn) ? undefined : 'Invalid phone number') : 'Phone number required'}
-                                />
-
-                                {
-                                    state.errors.msisdn.length > 0 &&
-                                    <span className='invalid-feedback text-xs text-red-600 pl-0'>
-                                        {state.errors.msisdn}
-                                    </span>
-                                }
-
-                                {
-                                    idC_State.error && (
-                                        <span className='invalid-feedback text-xs text-red-600 pl-0'>
-                                            {idC_State.error}
-                                        </span>
-                                    )
-                                }
-                            </div>
-
-                            <div className="mb-3 pt-3 px-3 md:px-0">
-                                <button className="bg-amber-600 float-right relative w-28 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:bg-amber-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:bg-amber-700" type="submit">
-                                    {
-                                        idC_State.processing ? (
-                                            <div className="flex justify-center items-center gap-3 py-2">
-                                                <i className="fad fa-spinner-third fa-xl fa-spin"></i>
-                                            </div>
-                                        ) : (
-                                            <div className="flex justify-center items-center gap-3">
-                                                Next
-                                            </div>
-                                        )
-                                    }
-                                </button>
-                            </div>
-                        </form>
+                        <div className="md:basis-2/5 hidden md:block h-screen px-4 py-8">
+                            <img className="h-full bg-amber-100 rounded-2xl" src={serviceCenter} alt={"pana_calling"} loading="lazy" />
+                        </div>
                     </div>
-                </div>
+                </section>
             </div>
         </React.Fragment>
     )
